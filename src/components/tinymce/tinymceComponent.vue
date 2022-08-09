7237861229da207d89163ee0c44b3e7077a57f67
李昶 <yr.muzi@gmail.com>
2022-08-09 22:39:45 +0800

    tinymce 组合式

--- src/components/tinymce/tinymceComponent.vue
+++ src/components/tinymce/tinymceComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="tinymce-box">
-        <editor v-model="textContent" :init="init" :disabled="disabled" @onClick="onClick"></editor>
+        <editor v-model="value" :init="init" :disabled="disabled" @onClick="onClick"></editor>
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import tinymce from 'tinymce/tinymce'; // tinymce默认hidden，不引入不显示
 import Editor from '@tinymce/tinymce-vue';
 import 'tinymce/themes/silver/theme'; // 主题文件
@@ -29,12 +29,14 @@ import 'tinymce/plugins/save'; // 保存
 import 'tinymce/plugins/searchreplace'; // 查询替换
 import 'tinymce/plugins/pagebreak'; // 分页
 import 'tinymce/plugins/insertdatetime';
+
+import { computed, defineComponent, onMounted } from 'vue';
 // 时间插入
-export default {
+export default defineComponent({
     name: 'tinymceComponent',
     components: { Editor },
     props: {
-        value: {
+        modelValue: {
             type: String,
             default: '',
         },
@@ -42,89 +44,77 @@ export default {
             type: Boolean,
             default: false,
         },
-        menubar: {
-            default: 'file edit insert view format table tools help',
+        plugins: {
+            type: [String, Array],
+            // default: 'lists image media table wordcount save preview',
+            // 插件需要import进来
+            // default: 'wordcount visualchars visualblocks toc textpattern template tabfocus spellchecker searchreplace save quickbars print preview paste pagebreak noneditable nonbreaking media insertdatetime importcss imagetools image hr help fullscreen fullpage directionality codesample code charmap link code table lists advlist anchor autolink autoresize autosave'
         },
-        // plugins: {
-        //     type: [String, Array],
-        //     default: 'lists image media table wordcount save preview',
-        //     // 插件需要import进来
-        //     // default: 'wordcount visualchars visualblocks toc textpattern template tabfocus spellchecker searchreplace save quickbars print preview paste pagebreak noneditable nonbreaking media insertdatetime importcss imagetools image hr help fullscreen fullpage directionality codesample code charmap link code table lists advlist anchor autolink autoresize autosave'
-        // },
-        // toolbar: {
-        //     type: [String, Array],
-        //     // default: 'undo redo |  formatselect | bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent blockquote | lists image media table | codesample code removeformat save preview'
-        //     default:
-        //         'formats undo redo paste print fontsizeselect fontselect template fullpage|wordcount ltr rtl visualchars visualblocks toc spellchecker searchreplace|save preview pagebreak nonbreaking|media image|outdent indent aligncenter alignleft alignright alignjustify lineheight  underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic  strikethrough hr charmap link insertdatetime|subscript superscript cut codesample code |anchor preview fullscreen|help',
-        // },
+        toolbar: {
+            type: [String, Array],
+            // default:
+            //     'undo redo |  formatselect | bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent blockquote | lists image media table | codesample code removeformat save preview',
+            // default:
+            //     'formats undo redo paste print fontsizeselect fontselect template fullpage|wordcount ltr rtl visualchars visualblocks toc spellchecker searchreplace|save preview pagebreak nonbreaking|media image|outdent indent aligncenter alignleft alignright alignjustify lineheight  underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic  strikethrough hr charmap link insertdatetime|subscript superscript cut codesample code |anchor preview fullscreen|help',
+        },
     },
-    data() {
-        return {
-            init: {
-                // width: 720,
-                height: 300,
-                language_url: '/resource/tinymce/langs/zh-Hans.js',
-                language: 'zh-Hans',
-                // 皮肤：这里引入的是public下的资源文件
-                skin_url: '/resource/tinymce/skins/ui/oxide',
+    // emits: ['update:modelValue'],
+    setup(props, ctx) {
+        const init = {
+            // width: 720,
+            height: 300,
+            language_url: '/resource/tinymce/langs/zh-Hans.js',
+            language: 'zh-Hans',
+            // 皮肤：这里引入的是public下的资源文件
+            skin_url: '/resource/tinymce/skins/ui/oxide',
 
-                // skin_url: 'tinymce/skins/ui/oxide-dark',//黑色系
-                plugins: this.plugins,
-                toolbar: this.toolbar,
-                content_css: '/resource/tinymce/skins/content/default/content.css',
-                branding: false,
-                // 隐藏菜单栏
-                menubar: this.menubar,
-                // 是否显示底部状态栏
-                statusbar: true,
-                // convert_urls: false,
-                // 初始化完成
-                init_instance_callback: (editor) => {
-                    console.log('初始化完成：', editor);
-                },
-                // 此处为图片上传处理函数，这个直接用了base64的图片形式上传图片，
-                // 如需ajax上传可参考https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_handler
-                images_upload_handler: (blobInfo, success) => {
-                    const img = `data:image/jpeg;base64,${blobInfo.base64()}`;
-                    console.log('图片上传处理：', img);
-                    success(img);
-                },
+            // skin_url: 'tinymce/skins/ui/oxide-dark',//黑色系
+            plugins: props.plugins,
+            toolbar: props.toolbar,
+            content_css: '/resource/tinymce/skins/content/default/content.css',
+            branding: false,
+            // 隐藏菜单栏
+            menubar: false,
+            // 是否显示底部状态栏
+            statusbar: true,
+            // convert_urls: false,
+            // 初始化完成
+            init_instance_callback: (editor) => {
+                console.log('初始化完成：', editor);
             },
-            textContent: this.value,
+            // 此处为图片上传处理函数，这个直接用了base64的图片形式上传图片，
+            // 如需ajax上传可参考https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_handler
+            images_upload_handler: (blobInfo, success) => {
+                const img = `data:image/jpeg;base64,${blobInfo.base64()}`;
+                console.log('图片上传处理：', img);
+                success(img);
+            },
         };
-    },
-    watch: {
-        value(newValue) {
-            this.textContent = newValue;
-        },
-        textContent(newValue) {
-            // 自定义事件给父组件传递数据
-            this.$emit('input', newValue);
-        },
-    },
-    // 实例创建完成
-    // 组件挂载完毕
-    mounted() {
-        // 初始化 tinymce
-        tinymce.init({});
-    },
-    methods: {
+        const value = computed({
+            get() {
+                return props.modelValue;
+            },
+            set(val) {
+                ctx.emit('update:modelValue', val);
+            },
+        });
+
+        function clear() {
+            value.value = '';
+        }
+        // 自定义事件给父组件传递数据
+
         // 添加相关的事件，可用的事件参照文档=> https://github.com/tinymce/tinymce-vue => All available events
         // 需要什么事件可以自己增加
-        onClick(e) {
-            // 自定义事件给父组件传递数据
-            this.$emit('onClick', e, tinymce);
-        },
-        // 可以添加一些自己的自定义事件，如清空内容
-        clear() {
-            this.textContent = '';
-        },
+
+        onMounted(() => {
+            // 初始化 tinymce
+            tinymce.init({});
+        });
+
+        return { init, value, clear };
     },
-};
+});
 </script>
 
-<style scoped>
-.tinymce-box {
-    margin: 20px;
-}
-</style>
+<style scoped></style>
--- src/views/editPage.vue
+++ src/views/editPage.vue
@@ -3,12 +3,12 @@
  * @Author: 李昶
  * @Date: 2022-08-09 14:57:19
  * @LastEditors: 李昶
- * @LastEditTime: 2022-08-09 15:55:18
+ * @LastEditTime: 2022-08-09 22:38:15
  * @Profile: 一个比较废柴的前端开发
 -->
 <template>
     <div class="edit-wrapper">
-        <tinymce-component ref="editor" v-model="msg" :disabled="disabled" @onClick="onClick" />
+        <tinymce-component ref="editor" v-model="msg" :disabled="disabled" />
     </div>
 </template>
 
@@ -25,12 +25,6 @@ export default {
         };
     },
     methods: {
-        // 鼠标单击的事件
-        onClick(e, editor) {
-            console.log('Element clicked');
-            console.log(e);
-            console.log(editor);
-        },
         // 清空内容
         clear() {
             this.$refs.editor.clear();
