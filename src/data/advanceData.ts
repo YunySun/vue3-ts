950a3483c7ced32db76366c23b03702e06d8ecba
李昶 <yr.muzi@gmail.com>
2022-09-15 23:35:46 +0800

    bind

--- src/data/advanceData.ts
+++ src/data/advanceData.ts
@@ -3,7 +3,7 @@
  * @Author: 李昶
  * @Date: 2022-09-14 11:17:37
  * @LastEditors: 李昶
- * @LastEditTime: 2022-09-15 17:07:12
+ * @LastEditTime: 2022-09-15 23:24:29
  * @Profile: 一个比较废柴的前端开发
  */
 const demo1 = `function func(x, y, z) {
@@ -74,8 +74,39 @@ let obj = {
 
 console.log(func.myApply(obj, [1, 2, 3]));`;
 
+const demo4 = `
+
+Function.prototype.myBind = function (ctx) {
+    if (typeof this !== 'function') {
+        throw new TypeError('Type Error');
+    }
+    let args = [...arguments].slice(1);
+    ctx = ctx || window;
+    ctx.fn = this;
+    return function () {
+        let funcArgs = args.concat([...arguments]);
+        console.log(funcArgs);
+        let result = ctx.fn(...funcArgs);
+        delete ctx.fn;
+        return result;
+    };
+};
+
+function func(x, y, z, a, b) {
+    console.log(x, y, z, a, b);
+    console.log(this);
+    return this.sound;
+}
+
+let obj = {
+    sound: '你好',
+};
+
+console.log(func.myBind(obj, 1, 2, 3)(4, 5));`;
+
 export default {
     demo1,
     demo2,
     demo3,
+    demo4,
 };
--- src/views/interview/advancePage.vue
+++ src/views/interview/advancePage.vue
@@ -3,7 +3,7 @@
  * @Author: 李昶
  * @Date: 2022-09-13 13:59:09
  * @LastEditors: 李昶
- * @LastEditTime: 2022-09-15 17:18:41
+ * @LastEditTime: 2022-09-15 23:25:21
  * @Profile: 一个比较废柴的前端开发
 -->
 <template>
@@ -24,7 +24,8 @@
         <p><code-txt>apply</code-txt>函数相对于<code-txt>call</code-txt>的区别只是在传参上有所不同，传的为数组。</p>
         <highlightjs lang="js" :code="demo3" />
         <h3>bind</h3>
-        <p><code-txt>bind</code-txt>的区别是不直接调用，而是直接返回一个函数。</p>
+        <p><code-txt>bind</code-txt>的区别是不直接调用，而是直接返回一个函数。并且返回的函数可以继续传值。</p>
+        <highlightjs lang="js" :code="demo4" />
     </div>
 </template>
 
@@ -32,34 +33,6 @@
 import { defineComponent } from 'vue';
 import data from '@/data/advanceData';
 import codeTxt from '@/components/tools/codeTxt.vue';
-
-Function.prototype.myBind = function (ctx) {
-    // this 是调用myCall的函数 this必须是函数
-    if (typeof this !== 'function') {
-        throw new TypeError('Type Error');
-    }
-    let args = [...arguments].slice(1);
-    // 改变调用myCall函数的this
-    // this(...args); // 这时候函数的this是window
-    // 函数谁调用 this就指向谁 在没有传参的基础上  函数指向为window
-    ctx = ctx || window;
-    ctx.fn = this; // 将函数赋值给调用myCall函数的一个属性上
-    let result = ctx.fn(args);
-    delete ctx.fn;
-    return result;
-};
-
-function func(x, y, z) {
-    console.log(x, y, z);
-    console.log(this);
-    return this.sound;
-}
-
-let obj = {
-    sound: '你好',
-};
-
-console.log(func.myBind(obj, 1, 2, 3));
 
 export default defineComponent({
     components: { codeTxt },
