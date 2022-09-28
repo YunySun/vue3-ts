f4df400a6dabc5e538abbccc22df6bba283876c3
李昶 <yr.muzi@gmail.com>
2022-09-28 23:28:13 +0800

    基础知识点

---
+++ src/data/rust/basicData.ts
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: 李昶
+ * @Date: 2022-09-28 23:02:50
+ * @LastEditors: 李昶
+ * @LastEditTime: 2022-09-28 23:02:56
+ * @Profile: 一个比较废柴的前端开发
+ */
+const demo1 = `struct Connection { 
+    io: S,
+    state: State,
+}`;
+
+export default {
+    demo1,
+};
