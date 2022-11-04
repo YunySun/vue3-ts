21bcea4ed9324f15d4f41b22f27944eb25a898ef
李昶 <48754120+YunySun@users.noreply.github.com>
2022-11-04 17:17:00 +0800

    trait练习  关联类型trait 泛型trait

--- src/data/rust/07BasicData.ts
+++ src/data/rust/07BasicData.ts
@@ -3,7 +3,7 @@
  * @Author: 李昶
  * @Date: 2022-11-03 16:11:25
  * @LastEditors: 李昶
- * @LastEditTime: 2022-11-03 22:34:30
+ * @LastEditTime: 2022-11-04 17:11:59
  * @Profile: 一个比较废柴的前端开发
  */
 const demo1 = `pub trait Write {
@@ -69,6 +69,163 @@ fn main() {
     println!("{:?}", buf);
 }`;
 
+const demo3 = `pub trait Parse {
+    fn parse(s: &str) -> Self;
+}`;
+
+const demo4 = `use regex::Regex;
+
+pub trait Parse {
+    fn parse(s: &str) -> Self;
+}
+
+impl Parse for u8 {
+    fn parse(s: &str) -> Self {
+        let re: Regex = Regex::new(r"^[0-9]+").unwrap();
+        if let Some(captures) = re.captures(s) {
+            captures
+                .get(0)
+                .map_or(0, |s| s.as_str().parse().unwrap_or(0))
+        } else {
+            0
+        }
+    }
+}
+
+#[test]
+fn parse_should_work() {
+    assert_eq!(u8::parse("123abcd"), 123);
+    assert_eq!(u8::parse("1234abcd"), 0);
+    assert_eq!(u8::parse("abcd"), 0);
+}
+
+fn main() {
+    println!("result: {}", u8::parse("255 hello world"));
+    println!("result: {}", f64::parse("255.52hello world"))
+}`;
+
+const demo5 = `impl Parse for f64 {
+    fn parse(s: &str) -> Self {
+        let re: Regex = Regex::new(r"[0-9]+\\.[0-9]+").unwrap();
+        if let Some(captures) = re.captures(s) {
+            captures
+                .get(0)
+                .map_or(0.0, |s| s.as_str().parse().unwrap_or(0.0))
+        } else {
+            0.0
+        }
+    }
+}`;
+
+const demo6 = `use regex::Regex;
+use std::str::FromStr;
+
+pub trait Parse {
+    fn parse(s: &str) -> Self;
+}
+
+// 约束T必须同时实现了FromStr和Default 否则Default::default作为缺省值报错
+impl<T> Parse for T
+where
+    T: FromStr + Default,
+{
+    fn parse(s: &str) -> Self {
+        let re: Regex = Regex::new(r"^[0-9]+(\\.[0-9]+)?").unwrap();
+        // 生成一个创建缺省值的闭包
+        // Default::default()返回的类型根据上下文能推导出来，是Self
+        // 约定了Self，T需要实现Default trait
+        let d = || Default::default();
+        if let Some(captures) = re.captures(s) {
+            captures
+                .get(0)
+                .map_or(d(), |s| s.as_str().parse().unwrap_or(d()))
+        } else {
+            d()
+        }
+    }
+}
+
+#[test]
+fn parse_should_work() {
+    assert_eq!(u8::parse("123abcd"), 123);
+    assert_eq!(u8::parse("1234abcd"), 0);
+    assert_eq!(u8::parse("abcd"), 0);
+}
+
+fn main() {
+    println!("result: {}", u8::parse("255 hello world"));
+    println!("result: {}", f64::parse("255.52hello world"));
+    println!("result: {}", f64::parse("abcd"))
+}`;
+
+const demo7 = `pub trait Parse {
+    fn parse(s: &str) -> Result<Self, E>; // error: cannot find type E in this scope
+}`;
+
+const demo8 = `pub trait Parse {
+    type Error;
+    fn parse(s: &str) -> Result<Self, Self::Error>; // the size for values of type Self cannot be known at compilation time 添加where Self: Sized;
+}`;
+
+const demo9 = `use regex::Regex;
+use std::str::FromStr;
+
+pub trait Parse {
+    type Error;
+    fn parse(s: &str) -> Result<Self, Self::Error>
+    where
+        Self: Sized;
+}
+
+// 约束T必须同时实现了FromStr和Default 否则Default::default作为缺省值报错
+impl<T> Parse for T
+where
+    T: FromStr + Default,
+{
+    // 定义关联类型Error为String
+    type Error = String;
+    fn parse(s: &str) -> Result<Self, Self::Error> {
+        let re: Regex = Regex::new(r"^[0-9]+(\\.[0-9]+)?").unwrap();
+        if let Some(captures) = re.captures(s) {
+            captures.get(0).map_or(Err("failed".to_string()), |s| {
+                s.as_str().parse().map_err(|_err| "failed".to_string())
+            })
+        } else {
+            Err("failed".to_string())
+        }
+    }
+}
+
+#[test]
+fn parse_should_work() {
+    assert_eq!(u8::parse("123abcd"), Ok(123));
+    assert_eq!(u32::parse("1234abcd"), Ok(1234));
+    assert_eq!(u8::parse("abcd"), Err("failed".into()));
+}
+
+fn main() {
+    println!("result: {:?}", u8::parse("255 hello world"));
+    println!("result: {:?}", f64::parse("255.52hello world"));
+    println!("result: {:?}", f64::parse("abcd"))
+}`;
+
+const demo10 = `pub trait Add<Rhs = Self> {
+    #[stable(feature = "rust1", since = "1.0.0")]
+    type Output;
+    #[must_use]
+    #[stable(feature = "rust1", since = "1.0.0")]
+    fn add(self, rhs: Rhs) -> Self::Output;
+}`;
+
 export default {
     demo1,
+    demo2,
+    demo3,
+    demo4,
+    demo5,
+    demo6,
+    demo7,
+    demo8,
+    demo9,
+    demo10,
 };
--- src/views/rust/07BasicPage.vue
+++ src/views/rust/07BasicPage.vue
@@ -3,7 +3,7 @@
  * @Author: 李昶
  * @Date: 2022-11-03 15:40:14
  * @LastEditors: 李昶
- * @LastEditTime: 2022-11-03 22:44:55
+ * @LastEditTime: 2022-11-04 17:14:53
  * @Profile: 一个比较废柴的前端开发
 -->
 <template>
@@ -35,6 +35,61 @@
             self和&[u8]，第一个参数传递的是buf这个变量的可变引用，第二个参数传递的是b"Hello world!"。
         </p>
         <h3 class="project__title">trait练习</h3>
+        <p class="article-tips">字符串的解析器，将字符串解析成自己希望的类型。</p>
+        <p>定义trait，有一个方法接受字符串引用，返回Self(ps:应该就是定义trait返回自己定义类型的值)</p>
+        <highlightjs lang="rust" :code="demo3" />
+        <p>所以之前说的缺省实现应该就是trait未定义的函数，而trait定义的函数如parse就是非缺省实现。</p>
+        <p>实现一个字符串将包含数字的字符串转换成数字</p>
+        <highlightjs lang="rust" :code="demo4" />
+        <p>实现字符串包含浮点型的字符串转换成浮点型f64</p>
+        <highlightjs lang="rust" :code="demo5" />
+        <p>
+            除了类型和用于捕获的regex略有变化外，整个代码基本和上面的代码是重复的。在实现trait的时候，也可以用泛型参数来实现trait，需要注意的是，要对泛型参数做一定的限制。
+        </p>
+        <ul>
+            <li>
+                第一，不是任何类型都可以通过字符串解析出来，在例子中，我们只能处理数字类型，并且这个类型还要能够被str::parse处理。
+            </li>
+            <li>
+                第二，上面代码当无法正确解析字符串的时候，会直接返回0，表示无法处理，但我们使用泛型参数后，无法返回0，因为0不一定是某个符合泛型参数的类型中的一个值。
+            </li>
+        </ul>
+        <p>
+            str::parse是一个泛型函数，它返回任何实现了FromStr
+            trait的类型，所以这里对泛型参数的第一个限制是，它必须实现了FromStr trait。
+        </p>
+        <p>
+            其实返回0的目的是为处理不了的情况，返回一个缺省值，在Rust标准库中有Default
+            trait，绝大多数类型都实现了这个trait，来为数据结构提供缺省值，所以泛型参数的另一个限制是Default。
+        </p>
+        <highlightjs lang="rust" :code="demo6" />
+        <p>
+            通过对带有约束的泛型参数实现trait，一份代码就实现了u32/f64等类型的Parse
+            trait，非常精简。但是这里返回缺省值的话，会跟解析“0abcd”这样的情况混淆，不知道解析出的0，究竟是出错了，还是本该解析出0。
+        </p>
+        <p>所以更好的方式是 parse 函数返回一个 Result</p>
+        <highlightjs lang="rust" :code="demo7" />
+        <p>
+            这里Result的E让人犯难了：要返回的错误信息，在trait定义时并不确定，不同的实现者可以使用不同的错误类型，这里trait的定义者最好能够把这种灵活性留给trait的实现者。
+        </p>
+        <h3 class="project__title">带关联类型的 trait</h3>
+        <p>Rust允许trait内部包含关联类型，实现时跟关联函数一样，它也需要实现关联类型。</p>
+        <highlightjs lang="rust" :code="demo8" />
+        <highlightjs lang="rust" :code="demo9" />
+        <p>
+            这种带有关联类型的trait比普通trait更加灵活，抽象度更高。trait方法的参数或者返回值，都可以用关联类型来表述，而在实现有关联类型的trait时，只需要额外提供关联类型的具体类型就可以了。
+        </p>
+        <h3 class="project__title">支持泛型的trait</h3>
+        <p class="article-tips">trait 的定义是不是也可以支持泛型呢？</p>
+        <p>
+            定义一个Contact
+            trait允许数据结构拼接起来，希望String和String、&str、可转换为String的数据进行拼接。如std::ops::Add这个提供加法运算的trait
+        </p>
+        <highlightjs lang="rust" :code="demo10" />
+        <p>
+            这个trait有一个泛型参数Rhs，代表的加号右边的值，用于add方法的第二个参数位。这个Rhs默认的是Self，也就是使用的Add
+            trait，如果不提供泛型参数，那么加号右值和左值都要是相同的类型。
+        </p>
     </div>
 </template>
 
